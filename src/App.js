8689ac05b84662e15fe53b0bfc7db5e5535ae54a
shbhmj <shbhmkmrjha@gmail.com>
2022-11-28 22:53:13 +0530

    API DEbugging

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-//Routing 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
+//Routing
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
-import Header from './components/Header';
-import Home from './components/Home';
-import Movie from './components/Movie';
-import NotFound from './components/NotFound';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 
 // Styles
-import { GlobalStyle } from './GlobalStyle';
+import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/:movieId' element={<Movie/>} />
-        <Route path='/*' element={<NotFound/>}/>
-      </Routes>
-      
-      <GlobalStyle />
-    </Router>
-  );
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/react-movies" element={<Home />} />
+      <Route path="/:movieId" element={<Movie />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 // Configuration for TMDB API
 // Read more about the API here: https://developers.themoviedb.org/
 
-const API_URL = "https://api.themoviedb.org/3";
+const API_URL = "https://api.themoviedb.org/3/";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const SEARCH_BASE_URL = `${API_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=`;
-const POPULAR_BASE_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`;
+const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
+const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
 // For login and voting
-const REQUEST_TOKEN_URL = `${API_URL}/authentication/token/new?api_key=${API_KEY}`;
-const LOGIN_URL = `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY}`;
-const SESSION_ID_URL = `${API_URL}/authentication/session/new?api_key=${API_KEY}`;
+const REQUEST_TOKEN_URL = `${API_URL}authentication/token/new?api_key=${API_KEY}`;
+const LOGIN_URL = `${API_URL}authentication/token/validate_with_login?api_key=${API_KEY}`;
+const SESSION_ID_URL = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
 
 const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 // Sizes: w300, w780, w1280, original
